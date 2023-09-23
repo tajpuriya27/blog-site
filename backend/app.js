94daca72935e93ffbf3a9665bda17655f0b01a78
tajpuriya27 <tajpuriyasunil27@gmail.com>
2023-09-23 15:28:43 +0545

    feat: mongoDB connection added

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const config = require("./utils/config");
+const logger = require("./utils/logger");
 const blogsRouter = require("./controllers/blogs");
 
+console.log("Connecting to ", config.mongoUrl);
+
+mongoose
+  .connect(config.mongoUrl)
+  .then(() => {
+    logger.info("Connected to MongoDB");
+  })
+  .catch((error) => {
+    logger.error("Error occured while connecting to DB.", error);
+  });
+
 app.use(express.json());
+
 app.use("/", blogsRouter);
 
 module.exports = app;
