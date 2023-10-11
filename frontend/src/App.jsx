fbfdca7ec0693dfae646e5396305d1a2a6cd0627
tajpuriya27 <tajpuriyasunil27@gmail.com>
2023-10-11 10:41:39 +0545

    feat: toast-container added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import { useSelector } from "react-redux";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "./main.css";
 
 import LoginPage from "./components/Login";
@@ -11,17 +13,36 @@ import BlogForm from "./components/BlogForm";
 const App = () => {
   const loggedIn = useSelector((state) => state.user);
   const isLoggedIn = Boolean(Object.keys(loggedIn).length);
+  const notify = () => toast("Log In to add new blog!!");
   console.log("loggedIn user", loggedIn);
 
   return (
     <>
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        limit={2}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/addblog" element={<BlogForm />} />
+        <Route
+          path="/addblog"
+          element={
+            isLoggedIn ? <BlogForm /> : notify() && <Navigate replace to="/" />
+          }
+        />
       </Routes>
     </>
   );
